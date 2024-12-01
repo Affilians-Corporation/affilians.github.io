090c2ebda90e1b76f6922ad52e2f84814bff585f
el_maximo <matteusmaximof@gmail.com>
2024-12-01 19:49:17 -0300

    TypeScript configuration

---
+++ chronoriot/src/index.js
@@ -0,0 +1,43 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var phaser_1 = require("phaser");
+var Example = /** @class */ (function (_super) {
+    __extends(Example, _super);
+    function Example() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Example.prototype.preload = function () {
+        this.load.setBaseURL("https://affilians.com.br/chronoriot");
+    };
+    Example.prototype.create = function () {
+    };
+    return Example;
+}(phaser_1.Scene));
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: Example,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x: -0, y: 200 }
+        }
+    }
+};
+var game = new Phaser.Game(config);
--- chronoriot/src/index.ts
+++ chronoriot/src/index.ts
@@ -1,15 +1,15 @@
-import Phaser, { Physics } from "phaser"
-class Example extends Phaser.Scene {
+import { Scene, Types } from "phaser"
+class Example extends Scene {
     preload () {
         this.load.setBaseURL("https://affilians.com.br/chronoriot")
     }
 
     create () {
-        
+
     }
 }
 
-const config = {
+const config: Types.Core.GameConfig = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
@@ -17,7 +17,7 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
+            gravity: { x: -0, y: 200 }
         }
     }
 };
