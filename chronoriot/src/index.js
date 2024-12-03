6c4ecf8575ab318bccd464d5162e440f4078b0a2
Matteus Maximo Felisberto <16165604+elmaximooficial@users.noreply.github.com>
2024-12-03 03:14:04 +0000

    Trying Phaser's builtin fullscreen toggle

--- chronoriot/src/index.js
+++ chronoriot/src/index.js
@@ -1,22 +1,9 @@
-function requestFullScreen(element) {
-    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
-
-    if (requestMethod) {
-        requestMethod.call(element);
-    } else if (typeof window.ActiveXObject !== "undefined") {
-        var wscript = new ActiveXObject("WScript.Shell");
-        if (wscript !== null) {
-            wscript.SendKeys("{F11}");
-        }
-    }
-}
-
-requestFullScreen(document.getElementById("game-container"))
-
 class Example extends Phaser.Scene {
     preload() {}
 
-    create() {}
+    create() {
+        this.scale.startFullscreen();
+    }
 }
 
 const config = {
