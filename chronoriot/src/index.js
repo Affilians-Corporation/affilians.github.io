def495d39b83478dafa3eee173c66dc0ba0a081e
Matteus Maximo Felisberto <16165604+elmaximooficial@users.noreply.github.com>
2024-12-03 00:33:22 +0000

    Trying fullscreen

--- chronoriot/src/index.js
+++ chronoriot/src/index.js
@@ -1,3 +1,18 @@
+function requestFullScreen(element) {
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) {
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") {
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+
+requestFullScreen(document.getElementsByTagName("body"))
+
 class Example extends Phaser.Scene {
     preload() {}
 
