394088f5bf9522333b25a2bdba81877388097fb5
Matteus Maximo Felisberto <16165604+elmaximooficial@users.noreply.github.com>
2024-12-03 03:03:38 +0000

    Setting up HTMX and Tailwind for the main website

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html, js}", "./src/*.{html, js}", "./res/*.{html, js}", "./components/*.{html, js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
