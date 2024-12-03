e57b67fe6ab363a82f272cef918c928301ac3447
Matteus Maximo Felisberto <16165604+elmaximooficial@users.noreply.github.com>
2024-12-03 00:23:55 +0000

    Initial Phaser JS commit

--- chronoriot/src/index.js
+++ chronoriot/src/index.js
@@ -1,45 +1,20 @@
-"use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-Object.defineProperty(exports, "__esModule", { value: true });
-var phaser_1 = require("phaser");
-var Example = /** @class */ (function (_super) {
-    __extends(Example, _super);
-    function Example() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    Example.prototype.preload = function () {
-        this.load.setBaseURL("https://affilians.com.br/chronoriot");
-        this.load.image("zombie", "../res/zombie.webp");
-    };
-    Example.prototype.create = function () {
-        this.add.image(400, 300, "zombie");
-    };
-    return Example;
-}(phaser_1.Scene));
-var config = {
+class Example extends Phaser.Scene {
+    preload() {}
+
+    create() {}
+}
+
+const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
     scene: Example,
     physics: {
-        default: 'arcade',
+        default: "arcade",
         arcade: {
-            gravity: { x: -0, y: 200 }
+            gravity: { y: 200 }
         }
     }
 };
-var game = new Phaser.Game(config);
+
+const game = new Phaser.Game(config);
