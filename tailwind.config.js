2bf4d6892d55145c4e9ec10197b883d63a109a0b
el_maximo <matteusmaximof@gmail.com>
2024-12-05 12:21:48 -0300

    5 December 2024 build

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["*.{html, js}", "./src/*.{html, js}", "./res/*.{html, js}", "./components/*.{html, js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
