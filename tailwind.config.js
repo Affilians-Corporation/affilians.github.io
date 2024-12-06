9268cbeab69a89c40ff55d399cb479f8e6ae60a9
matteusmaximo <matteusmaximo@fuvs.br>
2024-12-06 20:59:05 -0300

    Added login button, hover transition and chronoriot image

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,10 @@ module.exports = {
         extend: {},
         colors: {
             'primary': "#191919",
+            'primary-light': "#383838",
             'secondary': "#E6E6E6",
-            'accent': "#FFD700"
+            'accent': "#FFD700",
+            'accent-dark': "#D0B100"
         }
     },
     plugins: [],
